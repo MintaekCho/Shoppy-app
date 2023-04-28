859bc3e259e1b21c9d356ab8df367c8a4026168e
Mintaek_CHO <mintaek5555@gmail.com>
2023-04-28 12:57:37 +0900

    docs : 파이어베이스 로그인 상태관리 관련 주석 추가

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -39,7 +39,13 @@ export async function logout() {
     .catch(console.error);
 }
 
+
+
 //파이어베이스에서 유저가 로그인 되어있는 상태인지 세션상태를 관찰하여 알려주는 로직
+/**
+ * 유저에 대한 정보는 컴포넌트에서 보관하고 있는데 새로고침하면 컴포넌트의 상태도 리셋되기 때문에
+ * 유저가 로그인을 한 상태였는지 알 수가 없게 되버린다.
+ */
 export function onUserStateChange(callback) {
     onAuthStateChanged(auth, (user) => {
        callback(user);
